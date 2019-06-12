90c37a7113da9af0a9829d9945ad7de6855e8e08
Bruce Robinson <bprobins@Tommy-Hawk.local>
2019-06-12 10:45:38 -0700

    adding 4 api calls

--- search.sh
+++ search.sh
@@ -6,8 +6,25 @@ COMPANY=acme
 SVCPASSWORD=lolplaintext
 MOJAVE=153
 HIGHSIERRA=154
+SIERRA=155
+EOL=156
 
 #get record from advancedsearch API endpoint
-complist=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$MOJAVE --user $SVCACCOUNT:$SVCPASSWORD)
+mojavesearch=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$MOJAVE --user $SVCACCOUNT:$SVCPASSWORD)
 #parse out total quantity
-compid=$(echo $complist | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
+compid=$(echo $mojavesearch | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
+
+#get record from advancedsearch API endpoint
+highsierrasearch=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$HIGHSIERRA --user $SVCACCOUNT:$SVCPASSWORD)
+#parse out total quantity
+compid=$(echo $highsierrasearch | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
+
+#get record from advancedsearch API endpoint
+sierra=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$SIERRA --user $SVCACCOUNT:$SVCPASSWORD)
+#parse out total quantity
+compid=$(echo $sierra | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
+
+#get record from advancedsearch API endpoint
+eolsearch=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$EOL --user $SVCACCOUNT:$SVCPASSWORD)
+#parse out total quantity
+compid=$(echo $eolsearch | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
