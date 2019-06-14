4eb8d110cc3095886414e297f9620540749c36d5
Bruce Robinson <bayruce@gmail.com>
2019-06-13 18:41:07 -0700

    forgot JSSResource in link

--- search.sh
+++ search.sh
@@ -10,25 +10,25 @@ SIERRA=155
 EOL=156
 
 #get record from advancedsearch API endpoint
-mojavesearch=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$MOJAVE --user $SVCACCOUNT:$SVCPASSWORD)
+mojavesearch=$(curl https://$COMPANY.jamfcloud.com/JSSResource/advancedcomputersearches/id/$MOJAVE --user $SVCACCOUNT:$SVCPASSWORD)
 #parse out total quantity
 mojavenum=$(echo $mojavesearch | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
 echo $mojavenum > flatfiledb/mojave.txt
 
 #get record from advancedsearch API endpoint
-highsierrasearch=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$HIGHSIERRA --user $SVCACCOUNT:$SVCPASSWORD)
+highsierrasearch=$(curl https://$COMPANY.jamfcloud.com/JSSResource/advancedcomputersearches/id/$HIGHSIERRA --user $SVCACCOUNT:$SVCPASSWORD)
 #parse out total quantity
 highsierranum=$(echo $highsierrasearch | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
 echo $highsierranum > flatfiledb/highsierra.txt
 
 #get record from advancedsearch API endpoint
-sierra=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$SIERRA --user $SVCACCOUNT:$SVCPASSWORD)
+sierra=$(curl https://$COMPANY.jamfcloud.com/JSSResource/advancedcomputersearches/id/$SIERRA --user $SVCACCOUNT:$SVCPASSWORD)
 #parse out total quantity
 sierranum=$(echo $sierra | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
 echo $sierranum > flatfiledb/sierra.txt
 
 #get record from advancedsearch API endpoint
-eolsearch=$(curl https://$COMPANY.jamfcloud.com/advancedcomputersearches/id/$EOL --user $SVCACCOUNT:$SVCPASSWORD)
+eolsearch=$(curl https://$COMPANY.jamfcloud.com/JSSResource/advancedcomputersearches/id/$EOL --user $SVCACCOUNT:$SVCPASSWORD)
 #parse out total quantity
 eolnum=$(echo $eolsearch | xpath '//computers' 2>&1| awk -F'<size>|</size>' '{print $2}')
 echo $eolnum > flatfiledb/eol.txt
